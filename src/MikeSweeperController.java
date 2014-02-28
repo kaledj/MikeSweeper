9f44b410b33fe75c0d3c33e2d3b27ec5976bb40a
kaledj <kaledj2@gmail.com>
2014-02-27 19:19:31 -0500

    Added a controller class

---
+++ src/MikeSweeperController.java
@@ -0,0 +1,16 @@
+
+public class MikeSweeperController
+{
+    
+    private MikeSweeper model;
+    private MikeSweeperGui view;
+
+    public MikeSweeperController(MikeSweeper model, MikeSweeperGui view)
+    {
+        this.view = view;
+        this.model = model;
+    }
+    
+    
+
+}
--- src/MikeSweeperGui.java
+++ src/MikeSweeperGui.java
@@ -1,28 +1,32 @@
 import java.awt.EventQueue;
+import java.awt.GridLayout;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
-import java.awt.BorderLayout;
-import java.awt.GridLayout;
-import javax.swing.JButton;
-import javax.swing.JDesktopPane;
-import java.awt.FlowLayout;
 
 
-public class MikeSweeperGui {
+public class MikeSweeperGui
+{
 
     private JFrame frmMikesweeper;
 
     /**
      * Launch the application.
      */
-    public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                try {
+    public static void main(String[] args)
+    {
+        EventQueue.invokeLater(new Runnable()
+        {
+            public void run()
+            {
+                try
+                {
                     MikeSweeperGui window = new MikeSweeperGui();
                     window.frmMikesweeper.setVisible(true);
-                } catch (Exception e) {
+                }
+                catch (Exception e)
+                {
                     e.printStackTrace();
                 }
             }
@@ -32,31 +36,27 @@ public class MikeSweeperGui {
     /**
      * Create the application.
      */
-    public MikeSweeperGui() {
+    public MikeSweeperGui()
+    {
         initialize();
     }
 
     /**
      * Initialize the contents of the frame.
      */
-    private void initialize() {
+    private void initialize()
+    {
         frmMikesweeper = new JFrame();
         frmMikesweeper.setTitle("MikeSweeper");
-        frmMikesweeper.setBounds(100, 100, 497, 389);
+        frmMikesweeper.setBounds(100, 100, 450, 300);
         frmMikesweeper.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frmMikesweeper.getContentPane().setLayout(new GridLayout(0, 4, 0, 0));
+        frmMikesweeper.getContentPane().setLayout(new GridLayout(10, 10, 0, 0));
         
-        JPanel panel = new JPanel();
-        frmMikesweeper.getContentPane().add(panel);
-        
-        JPanel panel_1 = new JPanel();
-        frmMikesweeper.getContentPane().add(panel_1);
-        
-        JPanel panel_2 = new JPanel();
-        frmMikesweeper.getContentPane().add(panel_2);
-        
-        JPanel panel_3 = new JPanel();
-        frmMikesweeper.getContentPane().add(panel_3);
+        for(int i = 0; i < 10; i++) {
+            for(int j = 0; j < 10; j++) {
+                frmMikesweeper.getContentPane().add(new JPanel().add(new JLabel("(" + i + ", " + j + ")")));
+            }
+        }
     }
 
 }
