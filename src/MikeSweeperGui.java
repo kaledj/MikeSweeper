ccf0716b2659874649c4ed810c9ae9db64b6d830
scottshuffler <scottshuffler@gmail.com>
2014-02-28 11:14:57 -0500

    You can blow up mikes face now

--- src/MikeSweeperGui.java
+++ src/MikeSweeperGui.java
@@ -1,16 +1,22 @@
 import java.awt.EventQueue;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.AbstractButton;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JToggleButton;
 
 
-public class MikeSweeperGui
+public class MikeSweeperGui implements ActionListener
 {
+    Icon icon = new ImageIcon("resources/10x10.png");
+    Icon iconEx = new ImageIcon("resources/10x10ex.png");
 
     private JFrame frmMikesweeper;
 
@@ -51,17 +57,26 @@ public class MikeSweeperGui
     {
         frmMikesweeper = new JFrame();
         frmMikesweeper.setTitle("MikeSweeper");
-        frmMikesweeper.setBounds(100, 100, 580, 600);
+        frmMikesweeper.setBounds(100, 100, 400, 450);
         frmMikesweeper.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frmMikesweeper.getContentPane().setLayout(new GridLayout(10, 10, 0, 0));
         
-        Icon icon = new ImageIcon("resources/10x10.png");
+
+
         
         for(int i = 0; i < 10; i++) {
             for(int j = 0; j < 10; j++) {
-                frmMikesweeper.getContentPane().add(new JPanel().add(new JButton(icon)));
+            	JButton ij = new JButton(icon);
+            	ij.addActionListener(this);
+                frmMikesweeper.getContentPane().add(new JPanel().add(ij));
             }
         }
     }
 
+	@Override
+	public void actionPerformed(ActionEvent e) {
+        	((AbstractButton) e.getSource()).setIcon(iconEx);
+		
+	}
+	
 }
